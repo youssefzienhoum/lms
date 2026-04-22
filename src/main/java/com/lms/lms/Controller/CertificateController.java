563b5a823804db2807b23227d216c7563de64150
mostafa-seni <mostafaayan886@gmail.com>
2026-04-22 15:36:57 +0200

    feat: Add certificate generation endpoint for instructors; update authorization roles for certificate access

--- src/main/java/com/lms/lms/Controller/CertificateController.java
+++ src/main/java/com/lms/lms/Controller/CertificateController.java
@@ -1,5 +1,6 @@
 package com.lms.lms.Controller;
 
+import com.lms.lms.DTOS.CertificateRequestDto;
 import com.lms.lms.DTOS.CertificateResponse;
 import com.lms.lms.Services.CertificateService;
 import lombok.RequiredArgsConstructor;
@@ -8,17 +9,30 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/api/certificates")
 @RequiredArgsConstructor
-@PreAuthorize("hasRole('STUDENT')")
+
 public class CertificateController {
+
 
     private final CertificateService certificateService;
 
+    @PostMapping("/generate/{courseId}")
+    @PreAuthorize("hasRole('INSTRUCTOR')")
+    public ResponseEntity<CertificateResponse> generateCertificate(
+            @RequestBody Long courseId,
+            @RequestBody CertificateRequestDto requestDto) {
+        return ResponseEntity.ok(certificateService.generateCertificateForCourseCompletion(requestDto,  courseId));
+    }
+
     // View certificates
     @GetMapping
+    @PreAuthorize("hasRole('STUDENT')")
     public ResponseEntity<List<CertificateResponse>> getMyCertificates() {
         return ResponseEntity.ok(certificateService.getMyCertificates());
     }
